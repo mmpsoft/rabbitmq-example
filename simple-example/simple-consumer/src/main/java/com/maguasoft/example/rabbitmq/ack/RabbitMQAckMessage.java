904fa3dd1edf23907c384d86c4fc1039d0f3d157
jimmy <jimmmmystudio@163.com>
2023-11-13 09:09:06 +0800

    simple-example 的 Ack 处理

---
+++ simple-example/simple-consumer/src/main/java/com/maguasoft/example/rabbitmq/ack/RabbitMQAckMessage.java
@@ -0,0 +1,57 @@
+package com.maguasoft.example.rabbitmq.ack;
+
+import com.rabbitmq.client.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class RabbitMQAckMessage {
+
+    public void onMessage() throws Exception {
+        // 创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("172.27.153.46");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/"); // 虚拟主机，默认 /
+        connectionFactory.setUsername("test"); // 用户名
+        connectionFactory.setPassword("test"); // 密码
+
+        // 创建 Connection
+        // 创建 Channel
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+            /*
+              接收消息
+              queue: 队列名称
+              autoAck: 是否自动确认
+              callback: 回调处理的函数，有 DefaultConsumer 实现类
+             */
+        channel.basicConsume("ack.queue", true, new DefaultConsumer(channel) {
+            /**
+             * @param consumerTag 消息的标识
+             * @param envelope 消息的元数据，可以获取到交换机、路由key等信息
+             * @param properties 参数
+             * @param body 消息内容
+             */
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                try {
+                    log.info("consumerTag: {}", consumerTag);
+                    log.info("envelope.Exchange: {}", envelope.getExchange());
+                    log.info("envelope.RoutingKey: {}", envelope.getRoutingKey());
+                    log.info("properties: {}", properties);
+                    log.info("body: {}", new String(body));
+
+                    // 确认消费成功
+                    channel.basicAck(envelope.getDeliveryTag(), true);
+                } catch (Exception e) {
+                    // multiple: true表示拒绝所有DeliveryTag的消息, false表示只拒绝当前这条DeliveryTag的消息
+                    // requeue: true表示重新投递回原队列，false表示丢弃
+                    channel.basicNack(envelope.getDeliveryTag(), true, true);
+                }
+            }
+        });
+    }
+}
---
+++ simple-example/simple-producer/src/main/java/com/maguasoft/example/rabbitmq/ack/RabbitMQAckCallback.java
@@ -0,0 +1,92 @@
+package com.maguasoft.example.rabbitmq.ack;
+
+import com.rabbitmq.client.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class RabbitMQAckCallback {
+
+    public void sendMessage() {
+        // 创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("172.27.153.46");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/"); // 虚拟主机，默认 /
+        connectionFactory.setUsername("test"); // 用户名
+        connectionFactory.setPassword("test"); // 密码
+
+        // 创建 Connection
+        // 创建 Channel
+        try (Connection connection = connectionFactory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            // 在此通道上启用确认模式
+            channel.confirmSelect();
+
+            /*
+              创建 Queue
+              queue: 队列名称
+              durable: 是否持久化，当 MQ 重启之后，数据不会丢失。true: 是，false: 否
+              exclusive:
+                   是否独占，表示只能一个消费者来消费这个队列
+                   当 Connection 关闭时，是否删除队列
+              autoDelete: 是否自动删除，当没有 Consumer 时，自动删除掉
+              arguments: 参数
+             */
+            // 如果 simple.queue 队列不存在，则会创建该队列，如有已存在不会创建该队列
+            channel.queueDeclare("ack.queue", true, false, false, null);
+
+            // 添加消息的Confirm监听
+            channel.addConfirmListener(new ConfirmListener() {
+                @Override
+                public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                    log.info("deliveryTag: {} acked", deliveryTag);
+                }
+
+                @Override
+                public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                    log.info("deliveryTag: {} no acked", deliveryTag);
+                }
+            });
+
+            // 添加消息的 Return 监听
+            channel.addReturnListener(new ReturnListener() {
+                @Override
+                public void handleReturn(int replyCode, String replyText, String exchange,
+                                         String routingKey, AMQP.BasicProperties properties, byte[] body) {
+                    log.info("replyCode: {}, " +
+                            "replyText: {}, " +
+                            "exchange: {}, " +
+                            "routingKey: {}, " +
+                            "properties: {}, " +
+                            "body: {}",
+                            replyCode,
+                            replyText,
+                            exchange,
+                            routingKey,
+                            properties,
+                            new String(body));
+                }
+            });
+
+            /*
+              发送消息
+              exchange: 交换机名称，simple 模式下没有交换机，所以值为 ""，也就是默认值
+              routingKey: 路由名称，如果使用默认的exchange时，routingKey必须queue值一样
+              props: 参数
+              body: 消息的内容
+             */
+            // 因为有多个消费者，为了测试方便，for 循环生产消息
+            for (int index = 1; index <= 100; index++) {
+                channel.basicPublish("", "workqueues.queue", null,
+                        ("Hello Simple mode of RabbitMQ-" + index).getBytes(StandardCharsets.UTF_8));
+            }
+
+        } catch (Exception e) {
+            log.error("Cause by: ", e);
+        }
+    }
+}
